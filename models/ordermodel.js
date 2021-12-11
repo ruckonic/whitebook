d76ca7fadc54c0658c10109b16f45db7bda177ca
kuttim <kuttim.mikk@gmail.com>
2021-12-11 17:02:35 +0200

    add prettier & eslint

--- models/ordermodel.js
+++ models/ordermodel.js
@@ -1,71 +1,91 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
+var order = mongoose.model("order", {
+  _id: mongoose.ObjectId,
+  firstname: String,
+  lastname: String,
+  phonenumber: String,
+  street: String,
+  city: String,
+  zipcode: String,
+  booklink: String,
+  ordered: Boolean,
+});
 
+var url = process.env.DB || "mongodb://localhost:27017/library";
 
-var order=mongoose.model('order',{
-    _id:mongoose.ObjectId,
-    firstname:String,
-    lastname:String,
-    phonenumber:String,
-    street:String,
-    city:String,
-    zipcode:String,
-    booklink:String,
-    ordered:Boolean
-})
+exports.addorder = (
+  firstname,
+  lastname,
+  phonenumber,
+  street,
+  city,
+  zipcode,
+  booklink,
+  ordered
+) => {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => {
+        return order
+          .insertMany({
+            firstname: firstname,
+            lastname: lastname,
+            phonenumber: phonenumber,
+            street: street,
+            city: city,
+            zipcode: zipcode,
+            booklink: booklink,
+            ordered: ordered,
+          })
+          .then((order) => {
+            mongoose.disconnect();
+            resolve(order);
+            console.log(order);
+          })
+          .catch((err) => {
+            mongoose.disconnect();
+            reject(err);
+            console.log(err);
+          });
+      })
+      .then((err) => console.log(err));
+  });
+};
 
+exports.getallorders = () => {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => {
+        return order.find({});
+      })
+      .then((orders) => {
+        mongoose.disconnect;
+        resolve(orders);
+      })
+      .catch((err) => {
+        mongoose.disconnect();
+        reject(err);
+      });
+  }).catch((err) => reject(err));
+};
 
-var url=process.env.DB || 'mongodb://localhost:27017/library'
-
-exports.addorder=(firstname,lastname,phonenumber,street,city,zipcode,booklink,ordered)=>{
-    return new Promise((resolve,reject)=>{
-        mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
-            return order.insertMany({
-                firstname:firstname,
-                lastname:lastname,
-                phonenumber:phonenumber,
-                street:street,
-                city:city,
-                zipcode:zipcode,
-                booklink:booklink,
-                ordered:ordered
-            }).then((order)=>{
-                mongoose.disconnect()
-                resolve(order)
-                console.log(order)
-            }).catch((err)=>{
-                mongoose.disconnect()
-                reject(err)
-                console.log(err)
-            })
-        }).then(err=>console.log(err))
-    })
-}
-
-exports.getallorders=()=>{
-    return new Promise((resolve,reject)=>{
-        mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
-            return order.find({})
-        }).then((orders)=>{
-            mongoose.disconnect
-            resolve(orders)
-        }).catch((err)=>{
-            mongoose.disconnect()
-            reject(err)
-        })
-    }).catch(err=>reject(err))
-}
-
-exports.endorder=(id,end)=>{
-    return new Promise((resolve,reject)=>{
-        mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
-            return order.updateOne({_id:id},{ordered:end})
-        }).then((order)=>{
-            mongoose.disconnect
-            resolve(order)
-        }).catch((err)=>{
-            mongoose.disconnect()
-            reject(err)
-        })
-    }).catch(err=>reject(err))
-}
+exports.endorder = (id, end) => {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => {
+        return order.updateOne({ _id: id }, { ordered: end });
+      })
+      .then((order) => {
+        mongoose.disconnect;
+        resolve(order);
+      })
+      .catch((err) => {
+        mongoose.disconnect();
+        reject(err);
+      });
+  }).catch((err) => reject(err));
+};
--- routes/orderroute.js
+++ routes/orderroute.js
@@ -1,10 +1,10 @@
-const ordercontroller=require('../controllers/ordercontroller')
-const router=require('express').Router()
-const guardauth=require('./guardauth')
+const ordercontroller = require("../controllers/ordercontroller");
+const router = require("express").Router();
+const guardauth = require("./guardauth");
 
-router.get('/contact',ordercontroller.getorderpage)
-router.post('/addorder',ordercontroller.order)
-router.post('/endorder',guardauth.islogin,ordercontroller.endorder)
-router.get('/oldorders',guardauth.islogin,ordercontroller.getoldorders)
-router.get('/neworders',guardauth.islogin,ordercontroller.getneworders)
-module.exports=router
+router.get("/contact", ordercontroller.getorderpage);
+router.post("/addorder", ordercontroller.order);
+router.post("/endorder", guardauth.islogin, ordercontroller.endorder);
+router.get("/oldorders", guardauth.islogin, ordercontroller.getoldorders);
+router.get("/neworders", guardauth.islogin, ordercontroller.getneworders);
+module.exports = router;
